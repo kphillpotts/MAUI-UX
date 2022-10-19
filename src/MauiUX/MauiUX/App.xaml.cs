d9d3521406135d770b2f20ae35e7c64c75c65ca1
Kym Phillpotts <kphillpotts@gmail.com>
2022-10-19 23:16:43 +1100

    Initial Project Creation

---
+++ src/MauiUX/MauiUX/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiUX;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/MauiUX/MauiUX/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiUX;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
