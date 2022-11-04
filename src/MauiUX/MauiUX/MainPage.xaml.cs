1f5f23e48b71df9e6db1f3acd81ff65df4248f6e
Kym Phillpotts <kphillpotts@gmail.com>
2022-11-04 22:08:17 +1100

    A bit of code cleanup

--- src/MauiUX/MauiUX/MainPage.xaml.cs
+++ src/MauiUX/MauiUX/MainPage.xaml.cs
@@ -7,32 +7,32 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-    private async void OnTransformsClicked(object sender, EventArgs e)
+    private async void TransformsPage_Clicked(object sender, EventArgs e)
 	{
         await Navigation.PushAsync(new Pages.TransformsPage());
 	}
 
-	private async void Button_Clicked(object sender, EventArgs e)
+	private async void FontsPage_Clicked(object sender, EventArgs e)
 	{
 		await Navigation.PushAsync(new Pages.FontsPage());
 	}
 
-	private async void Button_Clicked_1(object sender, EventArgs e)
+	private async void CollectionPage_Clicked(object sender, EventArgs e)
 	{
         await Navigation.PushAsync(new Pages.CollectionPage());
     }
 
-    private async void Button_Clicked_2(object sender, EventArgs e)
+    private async void SkiaPage_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Pages.SkiaPage());
     }
 
-    private async void Button_Clicked_3(object sender, EventArgs e)
+    private async void LottiePage_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Pages.LottiePage());
     }
 
-    private async void Button_Clicked_Themes(object sender, EventArgs e)
+    private async void ThemPage_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Pages.ThemePage());
     }
--- src/MauiUX/MauiUX/Pages/CollectionPage.xaml.cs
+++ src/MauiUX/MauiUX/Pages/CollectionPage.xaml.cs
@@ -7,52 +7,36 @@ public partial class CollectionPage : ContentPage
 	public CollectionPage()
 	{
 		InitializeComponent();
-
         myCollection.ItemsSource = Data.DataService.GetData();
     }
 
-	private void Button_Clicked(object sender, EventArgs e)
+	private void Card1_Clicked(object sender, EventArgs e)
 	{
-        myCollection.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical)
-        {
-            ItemSpacing = 0
-        };
-
+        myCollection.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical);
         myCollection.ItemTemplate = (DataTemplate)Resources["Card1"];
     }
 
-	private void Button_Clicked_1(object sender, EventArgs e)
+	private void Card2_Clicked(object sender, EventArgs e)
 	{
-        myCollection.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical)
-        {
-            ItemSpacing = 0
-        };
+        myCollection.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical);
         myCollection.ItemTemplate = (DataTemplate)Resources["Card2"];
     }
-    private void Button_Clicked_2(object sender, EventArgs e)
+    
+    private void Card3_Clicked(object sender, EventArgs e)
     {
-        myCollection.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Horizontal)
-        {
-            ItemSpacing = 0
-        };
+        myCollection.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Horizontal);
         myCollection.ItemTemplate = (DataTemplate)Resources["Card3"];
     }
-    private void Button_Clicked_3(object sender, EventArgs e)
+    
+    private void Card4_Clicked(object sender, EventArgs e)
     {
-        myCollection.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical)
-        {
-            ItemSpacing = 0
-        };
-        myCollection.ItemTemplate = (DataTemplate)Resources["GradientCard"];
+        myCollection.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical);
+        myCollection.ItemTemplate = (DataTemplate)Resources["Card4"];
     }
 
-    private void Button_Clicked_4(object sender, EventArgs e)
+    private void Card5_Clicked(object sender, EventArgs e)
     {
-        myCollection.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical)
-        {
-            ItemSpacing = 0
-        };
-
-        myCollection.ItemTemplate = (DataTemplate)Resources["ColorCard"];
+        myCollection.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical);
+        myCollection.ItemTemplate = (DataTemplate)Resources["Card5"];
     }
 }
